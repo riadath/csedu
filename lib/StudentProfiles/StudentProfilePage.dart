69ae60861f17ef1e47816be75096802a2a1c5e23
riadath <akib152012@gmail.com>
2022-09-28 01:56:00 +0600

    student profile list page updated

--- lib/StudentProfiles/StudentProfilePage.dart
+++ lib/StudentProfiles/StudentProfilePage.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:csedu/RoundedInputField.dart';
+import 'package:csedu/StudentProfiles/User.dart';
 import 'package:csedu/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +13,6 @@ class StudentProfilePage extends StatefulWidget {
 class _StudentProfilePageState extends State<StudentProfilePage> {
   @override
   Widget build(BuildContext context) {
-    final t_controller = TextEditingController();
     return MaterialApp(
       title: 'student profile page',
       theme: ThemeData(
@@ -22,99 +21,46 @@ class _StudentProfilePageState extends State<StudentProfilePage> {
         primaryColorDark: Colors.grey[850],
         scaffoldBackgroundColor: const Color.fromARGB(255, 255, 255, 255),
       ),
-      home: Container(
-        child: Scaffold(
-          appBar: AppBar(
-            title: RoundedInputField(
-              hintText: '',
-              icon: Icons.near_me,
-              onChagned: (value) {},
-              controller: t_controller,
-            ),
-            actions: [
-              IconButton(
-                onPressed: () {
-                  createUser(name: t_controller.text);
-                },
-                icon: const Icon(
-                  Icons.add,
-                  color: gPrimaryColorDark,
-                ),
-              ),
-            ],
-          ),
-          body: StreamBuilder<List<User>>(
-            stream: readUsers(),
-            builder: ((context, snapshot) {
-              final users = snapshot.data;
-              if (snapshot.hasError) {
-                return Text('Could not get data >>> ${snapshot.error}');
-              } else if (snapshot.hasData) {
-                return ListView(
-                  children: users!.map(buildUser).toList(),
-                );
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            }),
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Student List'),
+        ),
+        body: StreamBuilder<List<User>>(
+          stream: readUsers(),
+          builder: ((context, snapshot) {
+            final users = snapshot.data;
+            if (snapshot.hasError) {
+              return Text('Could not get data >>> ${snapshot.error}');
+            } else if (snapshot.hasData) {
+              return ListView(
+                children: users!.map(buildUser).toList(),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
         ),
       ),
     );
   }
-
-  Future createUser({required String name}) async {
-    final docUser = FirebaseFirestore.instance.collection('users').doc();
-    final user = User(
-      id: docUser.id,
-      name: name,
-      batch: 56,
-      linkedIn: 'bd.linkedin.com',
-    );
-    final json = user.toJson();
-    await docUser.set(json);
-  }
-
-  Widget buildUser(User user) => ListTile(
-        leading: CircleAvatar(child: Text('${user.batch}')),
-        title: Text(user.name),
-        subtitle: Text(user.linkedIn),
-      );
 
   Stream<List<User>> readUsers() => FirebaseFirestore.instance
       .collection('users')
       .snapshots()
       .map((snapshot) =>
           snapshot.docs.map((doc) => User.fromJson(doc.data())).toList());
-}
 
-
-
-class User {
-  String id;
-  final String name;
-  final int? batch;
-  final String linkedIn;
-
-  User({
-    this.id = '',
-    required this.name,
-    required this.batch,
-    required this.linkedIn,
-  });
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'name': name,
-        'batch': batch,
-        'linkedIn': linkedIn,
-      };
-  static User fromJson(Map<String, dynamic> json) => User(
-        id: json['id'],
-        name: json['name'],
-        batch: json['batch'],
-        linkedIn: json['linkedIn'],
+  Widget buildUser(User user) => Card(
+        margin: const EdgeInsets.all(5),
+        shadowColor: Colors.lime,
+        color: Colors.grey[300],
+        child: ListTile(
+          leading: CircleAvatar(child: Text('${user.batch}')),
+          title: Text(user.name),
+          onTap: () {},
+          // subtitle: Text(user.linkedin_profile),
+        ),
       );
 }
