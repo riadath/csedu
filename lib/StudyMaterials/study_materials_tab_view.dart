245d2fc5e0ceaf950d8046991640625546aa33b5
Ittehad <chowdhuryittehad@gmail.com>
2022-10-15 21:52:29 +0600

    made tabview a stateful widget

--- lib/StudyMaterials/study_materials_tab_view.dart
+++ lib/StudyMaterials/study_materials_tab_view.dart
@@ -1,8 +1,15 @@
+import 'package:csedu/StudyMaterials/study_material_drawer.dart';
 import 'package:flutter/material.dart';
 
-class StudyMaterialTabView extends StatelessWidget {
+class StudyMaterialTabView extends StatefulWidget {
   const StudyMaterialTabView({Key? key}) : super(key: key);
 
+  @override
+  State<StudyMaterialTabView> createState() => _StudyMaterialTabViewState();
+}
+
+class _StudyMaterialTabViewState extends State<StudyMaterialTabView> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,18 +18,24 @@ class StudyMaterialTabView extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[800],
       ),
+      drawer : MainDrawer(),
       body: Container(),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() {
+          currentIndex = index;
+        }),
         items: const [
           BottomNavigationBarItem(
             icon: Icon( Icons.question_mark ),
             label: 'Past Questions',
-            backgroundColor: Colors.blue,
+            backgroundColor: Colors.red,
           ),
           BottomNavigationBarItem(
             icon: Icon( Icons.book ),
             label: 'Resources',
-            backgroundColor: Colors.blue,
+            backgroundColor: Colors.red,
           ),
         ],
       ),
